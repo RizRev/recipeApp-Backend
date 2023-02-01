f607890cc484fbd2b8414113fe93ce893f83c096
RizRev <rizkyrevanda@gmail.com>
2023-02-01 16:50:18 +0700

    update recipe predeploy 3

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(
 // app.use(cors());
 
 const corsOptions = {
-  origin: "https://magical-meringue-f7f79e.netlify.app/",
+  origin: "https://magical-meringue-f7f79e.netlify.app",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
